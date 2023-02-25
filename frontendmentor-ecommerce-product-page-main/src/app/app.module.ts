21aec21fa18b862a2c280a5483e5f8d1fdcad6d5
Leo-paul <leopaulmartin@hotmail.fr>
2023-02-26 00:41:56 +0100

    fix: removed core module and add cart component

--- frontendmentor-ecommerce-product-page-main/src/app/app.module.ts
+++ frontendmentor-ecommerce-product-page-main/src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
+import { CartComponent } from './cart/cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule
-  ],
+  declarations: [AppComponent, NavbarComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
