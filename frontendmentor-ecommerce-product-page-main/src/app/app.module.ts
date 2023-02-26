04c8533610df2093719caa576765a706043c0a15
Leo-paul <leopaulmartin@hotmail.fr>
2023-02-26 20:40:51 +0100

    feat: add sidenav component

--- frontendmentor-ecommerce-product-page-main/src/app/app.module.ts
+++ frontendmentor-ecommerce-product-page-main/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { ProductComponent } from './product/product/product.component';
 import { ProductFeatureCardComponent } from './product/product-feature-card/product-feature-card.component';
+import { SidenavComponent } from './navbar/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CartComponent, ProductComponent, ProductFeatureCardComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CartComponent,
+    ProductComponent,
+    ProductFeatureCardComponent,
+    SidenavComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
